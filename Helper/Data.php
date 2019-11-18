7f32a6025d03307384f378fdf1520cdcbfafc905
Kishan Savaliya <kishan.cybercom@gmail.com>
2019-11-18 13:01:25 +0530

    Corrected Helper Data file

--- Helper/Data.php
+++ Helper/Data.php
@@ -6,12 +6,20 @@ use \Magento\Store\Model\ScopeInterface;
 
 class Data extends AbstractHelper
 {
+	protected $_storeManager;
+
+	protected $_skProfilePic;
+
 	public function __construct(
-		\SK\ProfilePic\Block\Customer\Account\ProfilePic $skProfilePic
+		\SK\ProfilePic\Block\Customer\Account\ProfilePic $skProfilePic,
+		\Magento\Store\Model\StoreManagerInterface $storeManager
 	){
 		$this->_skProfilePic = $skProfilePic;
+		$this->_storeManager = $storeManager;
 	}
+
 	public function getProfilePicById($id){
-		return $this->_skProfilePic->getCustomer($id)->getSkProfilePic();
+		$mediaUrl = $this ->_storeManager-> getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA );
+		return $mediaUrl.'sk_profile_pic/'.$this->_skProfilePic->getCustomer($id)->getSkProfilePic();
 	}
 }
